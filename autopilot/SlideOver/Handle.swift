5d4fafa70e8aab2d941d1a2accf8556e9f7170fe
Matthew Molinar <matthew.a.molinar@gmail.com>
2022-11-28 12:13:09 -0600

    slide over card has 3 states that are not obstructive to the view of the app

---
+++ autopilot/SlideOver/Handle.swift
@@ -0,0 +1,25 @@
+//
+//  Handle.swift
+//  autopilot
+//
+//  Created by Molinar, Matthew A on 11/28/22.
+//
+
+import SwiftUI
+
+struct Handle : View {
+    private let handleThickness = CGFloat(5.0)
+    var body: some View {
+        RoundedRectangle(cornerRadius: handleThickness / 2.0)
+            .frame(width: 40, height: handleThickness)
+            .foregroundColor(Color.secondary)
+            .padding(5)
+    }
+}
+
+struct Handle_Previews: PreviewProvider {
+    static var previews: some View {
+        Handle()
+    }
+}
+
