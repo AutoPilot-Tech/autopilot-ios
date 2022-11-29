9e29414de1ebfe7a223607980abac9693e273d45
Matthew Molinar <matthew.a.molinar@gmail.com>
2022-11-29 17:42:04 -0600

    arc cell

--- autopilot/Arcs/ArcCell.swift
+++ autopilot/Arcs/ArcCell.swift
@@ -6,15 +6,53 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ArcCell: View {
+    let arc: Arc
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+            VStack(alignment: .leading) {
+                HStack(alignment: .top, spacing: 12) {
+                    KFImage(URL(string: arc.profileImageUrl))
+                        .resizable()
+                        .scaledToFill()
+                        .clipped()
+                        .frame(width: 56, height: 56)
+                        .cornerRadius(56 / 2)
+                        .padding(.leading)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        HStack {
+                            Text(arc.fullname)
+                                .font(.system(size: 14, weight: .semibold))
+                                .foregroundColor(.black)
+                            
+                            Text("@\(arc.username) •")
+                                .foregroundColor(.gray)
+                            
+                            Text(arc.timestampString)
+                                .foregroundColor(.gray)
+                        }
+                        
+                        Text(arc.description)
+                            .foregroundColor(.black)
+                    }
+                }
+                .padding(.bottom)
+                .padding(.trailing)
+                
+//                ArcActionsView(arc: arc)
+                
+                Divider()
+            }
+            .padding(.leading, -16)
+        }
 }
 
-struct ArcCell_Previews: PreviewProvider {
-    static var previews: some View {
-        ArcCell()
-    }
-}
+//struct ArcCell_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ArcCell(arc: )
+//    }
+//}
