0b9bedd929ed47ea938c728a2823c12227b76261
Matthew Molinar <matthew.a.molinar@gmail.com>
2022-11-30 21:14:09 -0600

    working on ArcMode

---
+++ autopilot/Views/Arc/ArcMode/ArcModeView.swift
@@ -0,0 +1,44 @@
+//
+//  ArcMode.swift
+//  autopilot
+//
+//  Created by Matthew Molinar on 11/30/22.
+//
+
+import SwiftUI
+
+struct ArcModeView: View {
+//    @EnvironmentObject var workoutOverview: OverviewStore
+    @State private var showOverview = false
+//    @State private var sh
+    
+    var body: some View {
+        ZStack {
+            Text("Arc Mode")
+            SlideOverCard {
+                ZStack(alignment: .topTrailing) {
+                    
+                    GeometryReader { geometry in
+                        VStack {
+                            
+                            // for some reason Handle needs to go here... ideally it would need to be inside the SlideOverCard definition.
+                            Handle()
+                            
+                            // Tab Bar here
+                            ScrollView {
+                                
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ArcModeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArcModeView()
+    }
+}
