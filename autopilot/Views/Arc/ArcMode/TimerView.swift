3139fb4a2ba8ec7ec3496ad8f299f8a29e8201de
molinar1999 <matthew.a.molinar@gmail.com>
2022-12-02 18:40:16 -0600

    Timer counts up

--- autopilot/Views/Arc/ArcMode/TimerView.swift
+++ autopilot/Views/Arc/ArcMode/TimerView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct TimerView: View {
+    @State private var currentTime = 0
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
         // new timer view
+        Text("\(currentTime)")
+            .font(.system(size: 45, design: .rounded))
+            .padding()
+            .onReceive(timer) { _ in
+                if self.currentTime > 0 {
+                    self.currentTime += 1
+                }
+                
+            }
     }
 }
 
