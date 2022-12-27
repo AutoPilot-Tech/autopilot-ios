bfcd4402fc27124356086a67a66f0140e2a73c58
molinar1999 <matthew.a.molinar@gmail.com>
2022-12-26 22:00:12 -0600

    exercise options enum

--- autopilot/ViewModel/ArcMode/UploadToDoTrainingArcViewModel.swift
+++ autopilot/ViewModel/ArcMode/UploadToDoTrainingArcViewModel.swift
@@ -8,7 +8,44 @@
 import SwiftUI
 import Firebase
 
+enum ExerciseOptions: Int, CaseIterable {
+    case wallPushUps
+    case vUps
+    case tripodDumbbellRows
+    case stepUps
+    case sidePlank
+    case seatedDumbbellOverhandPressNeutralGrip
+    
+    var exerciseName: String {
+        switch self {
+        case .wallPushUps: return "Wall Push Ups"
+        case .vUps: return "V-ups"
+        case .tripodDumbbellRows: return "Tripod Dumbbell Rows"
+        case .stepUps: return "Step ups"
+        case .sidePlank: return "Side Plank"
+        case .seatedDumbbellOverhandPressNeutralGrip: return "Seated Dumbbell Overhand Press Neutral Grip"
+    
+        }
+    }
+    
+    var videoName: String {
+        switch self {
+        case .wallPushUps: return "Wall-Push-Ups"
+        case .vUps: return "V-Ups"
+        case .tripodDumbbellRows: return "Tripod-Dumbbell-Rows"
+        case .stepUps: return "Step-Ups"
+        case .sidePlank: return "Side Plank"
+        case .seatedDumbbellOverhandPressNeutralGrip: return "Seated-Dumbbell-Overhand-Press-Neutral-Grip"
+    
+            
+            
+        }
+    }
+}
+
 // Upload ToDo Training Arc
 class UploadToDoTrainingArcViewModel: ObservableObject {
+    
+    
     
 }
