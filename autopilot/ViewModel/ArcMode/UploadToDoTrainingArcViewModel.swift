19348b66a4dd971bebb1c7d9c0035617f36ac725
molinar1999 <matthew.a.molinar@gmail.com>
2022-12-26 21:39:26 -0600

    made ToDoTrainingArcViewModel file

---
+++ autopilot/ViewModel/ArcMode/UploadToDoTrainingArcViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  UploadToDoTrainingArcViewModel.swift
+//  autopilot
+//
+//  Created by Matthew Molinar on 12/26/22.
+//
+
+import SwiftUI
+import Firebase
+
+// Upload ToDo Training Arc
+class UploadToDoTrainingArcViewModel: ObservableObject {
+    
+}
