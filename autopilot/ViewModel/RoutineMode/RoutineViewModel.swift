32f6ae01eb62ae3cf93746b6bc7c301061f61a71
molinar1999 <matthew.a.molinar@gmail.com>
2022-12-30 12:38:46 -0600

    Remove RoutineViewModel

--- autopilot/ViewModel/RoutineMode/RoutineViewModel.swift
+++ autopilot/ViewModel/RoutineMode/RoutineViewModel.swift
@@ -7,20 +7,20 @@
 
 import SwiftUI
 
-class RoutineViewModel: ObservableObject {
-    @Published var routine: Routine
-    @Published var isPaused = false
-    @Published var isQuitPromptShowing = false
-    @Published var isBottomSheetShowing = false
-    
-    
-    init(routine: Routine) {
-            self.routine = routine
-        }
-    
-    func saveRoutine() {
-            routine.save()
-        }
-    
-}
+//class RoutineViewModel: ObservableObject {
+//    @Published var routine: Routine
+//    @Published var isPaused = false
+//    @Published var isQuitPromptShowing = false
+//    @Published var isBottomSheetShowing = false
+//    
+//    
+//    init(routine: Routine) {
+//            self.routine = routine
+//        }
+//    
+//    func saveRoutine() {
+//            routine.save()
+//        }
+//    
+//}
 
